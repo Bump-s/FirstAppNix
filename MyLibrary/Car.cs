d52f5db688053c754663417afb7fbd258b2b1eb2
Viacheslav Kharchenko <v.a.kharchenko96@gmail.com>
2021-07-15 12:24:25 +0300

    Add project files.

---
+++ MyLibrary/Car.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLibrary
+{
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Car(string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+
+        public double СalculateOverclocking(double weight, int horsePower)
+        {
+            return weight * horsePower;
+        }
+    }
+}
---
+++ SecondProject/AssembyClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyProject
+{
+    class AssembyClass
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(@"C:\Users\Viacheslav\source\repos\FirstAppNix\MyLibrary\bin\Debug\netcoreapp3.1\MyLibrary.dll");
+            Console.WriteLine(assembly.FullName);
+
+            Type[] types = assembly.GetTypes();
+            foreach (Type t in types)
+            {
+                Console.WriteLine(t.Name);
+            }
+
+            try
+            {
+                Type type = assembly.GetType("MyLibrary.Car", true, true);
+                object obj = Activator.CreateInstance(type);
+                MethodInfo method = type.GetMethod("СalculateOverclocking");
+                object result = method.Invoke(obj, new object[] { 850, 520 });
+                Console.WriteLine((result));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
